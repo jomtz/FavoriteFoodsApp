654772d2989aa2630da776237ab8cb516e634148
josuemnez <josue@josuemartinez.com>
2019-07-10 19:34:41 -0400

    Set up files

---
+++ app/src/main/java/com/josuemartinez/favoritefoodsapp/FoodsBox.java
@@ -0,0 +1,26 @@
+package com.josuemartinez.favoritefoodsapp;
+
+public class FoodsBox {
+
+    public static String[] getFoodsNames() {
+        return new String[] {
+                "Pizza",
+                "Bacon",
+                "Steak",
+                "Ice Cream",
+                "Chocolate",
+                "Pasta & Noodles",
+                "Sushi",
+                "Oyster",
+                "Chicken",
+                "Candy and Chocolate",
+                "Grilled cheese",
+                "Beef",
+                "Soup",
+                "Hamburger",
+                "Pancake",
+                "Salmon",
+                "Turkey meat"
+        };
+    }
+}
