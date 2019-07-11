ce4bc24d543caffdb7081b416c370adf654c40c1
josuemnez <josue@josuemartinez.com>
2019-07-10 20:10:49 -0400

    Add Strings to TextView

--- app/src/main/java/com/josuemartinez/favoritefoodsapp/MainActivity.java
+++ app/src/main/java/com/josuemartinez/favoritefoodsapp/MainActivity.java
@@ -2,12 +2,25 @@ package com.josuemartinez.favoritefoodsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mFoodsListTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Use findViewById to get a reference to the TextView from the layout
+        mFoodsListTextView = findViewById(R.id.foods_list_names);
+
+        String[] foodsNames = FoodsBox.getFoodsNames();
+
+        for (String foodsName : foodsNames){
+            mFoodsListTextView.append(foodsName + "\n\n\n");
+        }
+
     }
 }
